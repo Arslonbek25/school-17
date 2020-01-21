406b2622965dfdea921f2faab01fc8d46e010cc1
Arslonbek <47861284+Arslonbek25@users.noreply.github.com>
2020-01-21 19:50:10 +0500

    Last commit

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ $(document).ready(function() {
         nav: true,
         center: true,
         loop: true,
-        dots: true,
         margin: 20,
         responsive: {
             0: {
@@ -26,4 +25,38 @@ $(document).ready(function() {
             }
         }
     });
+});
+
+$('.send').click((e) => {
+    let data = {
+        name: $('#name').val(),
+        surname: $('#surname').val(),
+        age: $('#age').val(),
+        phone: $('#phone').val(),
+        message: $('#message').val()
+    }
+
+    if (localStorage.getItem('lastSend') + 10 > new Date().getMinutes()) {
+        alert('Вы отправляете слишком много запросов, подождите несколько минут, прежде чем отправлять новый');
+        return false;
+    } else if (data.name <= 3 || data.surname <= 3 || age <= 6 && data.age >= 16 || data.phone < 10) {
+        alert('Введены неправильные данные');
+        return false;
+    }
+
+    e.preventDefault();
+
+
+    let message = `${data.name} ${data.surname}%0A${data.age} лет %0A${data.phone}%0A${data.message}`;
+
+    const token = '848021510:AAHuRhhxMCSTy1cuzmfAxH116SYbgsLfYNw';
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=331953675&text=${message}`;
+
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', url, true);
+    xhttp.send();
+
+    localStorage.setItem('lastSend', new Date().getMinutes());
 });
