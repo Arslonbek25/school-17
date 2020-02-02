76e5ea9de662d502bc4b0665da4747d22b88f0cc
Arslonbek <47861284+Arslonbek25@users.noreply.github.com>
2020-02-02 19:57:58 +0500

    Minor updates

--- js/script.js
+++ js/script.js
@@ -34,8 +34,14 @@ $('.send').click((e) => {
 });
 
 $('body').on('scroll', function() {
-    if($('body').scrollTop() > 50) {
+    if ($('body').scrollTop() > 50) {
         $('.nav').addClass('nav--scrolled');
-    } else 
-    $('.nav').removeClass('nav--scrolled');
+    } else
+        $('.nav').removeClass('nav--scrolled');
 });
+
+$('#scroll-up').on('click', function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 'slow');
+})
